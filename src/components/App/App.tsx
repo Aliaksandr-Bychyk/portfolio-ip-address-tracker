6bcb7e51ec05083202fe60779ab31c5af0c8c79b
Aliaksandr <sanya55bychyk@gmail.com>
2023-10-26 14:31:40 +0200

    feat: add vite + react + ts

---
+++ src/components/App/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+    IP Address Tracker
+  
+    Search for any IP address or domain
+  
+    IP Address
+    Location
+    Timezone
+      UTC {/* add offset value dynamically using the API -->*/}
+    ISP
+    
+    </>
+  )
+}
+
+export default App
