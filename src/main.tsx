b8f366eae2c4b7cfd33b3628f29b6d2764a270a0
Aliaksandr <sanya55bychyk@gmail.com>
2023-10-31 01:03:46 +0100

    feat: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './components/App/App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App.tsx';
+import './index.css';
+import Attribution from './components/Attribution/Attribution.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <Attribution />
+  </React.StrictMode>
+);
