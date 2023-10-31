5b0121445591499e39052121f9f00f3bf1fea6de
Aliaksandr <sanya55bychyk@gmail.com>
2023-10-31 01:04:37 +0100

    feat: update App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,12 @@
-import './App.css'
+import Background from '../Background/Background.tsx';
+import Layout from '../Layout/Layout.tsx';
+import './App.css';
 
-function App() {
+const App = () => (
+  <div className="app">
+    <Layout />
+    <Background />
+  </div>
+);
 
-  return (
-    <>
-
-    IP Address Tracker
-  
-    Search for any IP address or domain
-  
-    IP Address
-    Location
-    Timezone
-      UTC {/* add offset value dynamically using the API -->*/}
-    ISP
-    
-    </>
-  )
-}
-
-export default App
+export default App;
