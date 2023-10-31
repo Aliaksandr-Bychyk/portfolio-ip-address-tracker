35c33a627c39f265852f91bb37483e5f1368293a
Aliaksandr <sanya55bychyk@gmail.com>
2023-10-31 01:03:24 +0100

    feat: add Attribution component

---
+++ src/components/Attribution/Attribution.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+
+const Attribution: FC = () => (
+  <div className="attribution">
+    Challenge by{' '}
+    <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+      Frontend Mentor
+    </a>
+    . Coded by <a href="#">Your Name Here</a>.
+  </div>
+);
+
+export default Attribution;
