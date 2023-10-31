9f748cc96db41cd11587b25ed71a7e93baf67633
Aliaksandr <sanya55bychyk@gmail.com>
2023-10-31 01:04:24 +0100

    feat: add Background component

---
+++ src/components/Background/Background.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import './Background.css';
+
+const Background: FC = () => (
+  <>
+    <div className="background" />
+    <div className="map"></div>
+  </>
+);
+
+export default Background;
