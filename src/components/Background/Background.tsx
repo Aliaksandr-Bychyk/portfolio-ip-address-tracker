7a0aa342503bb7c3f3dddf02de90f8db68a32fd5
Aliaksandr <sanya55bychyk@gmail.com>
2023-11-01 17:17:37 +0100

    feat: add leaflet map

--- src/components/Background/Background.tsx
+++ src/components/Background/Background.tsx
@@ -1,11 +1,26 @@
 import { FC } from 'react';
 import './Background.css';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { Icon } from 'leaflet';
+import markerIcon from '../../assets/icon-location.svg';
 
-const Background: FC = () => (
-  <>
-    <div className="background" />
-    <div className="map"></div>
-  </>
-);
+const Background: FC = () => {
+  return (
+    <>
+      <div className="background" />
+      <MapContainer center={[52.2323, 21.0061]} zoom={13} scrollWheelZoom={false} className="map">
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker
+          position={[52.2323, 21.0061]}
+          icon={new Icon({ iconUrl: markerIcon, iconAnchor: [23, 56] })}
+        />
+      </MapContainer>
+    </>
+  );
+};
 
 export default Background;
