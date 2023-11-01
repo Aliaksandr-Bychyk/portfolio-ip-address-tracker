de0fe55e3ddcd64222f23d178bcf33b0b63c41a6
Aliaksandr <sanya55bychyk@gmail.com>
2023-11-01 17:19:28 +0100

    feat: update attribution

--- src/components/Attribution/Attribution.tsx
+++ src/components/Attribution/Attribution.tsx
@@ -6,7 +6,11 @@ const Attribution: FC = () => (
     <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
       Frontend Mentor
     </a>
-    . Coded by <a href="#">Your Name Here</a>.
+    . Coded by{' '}
+    <a href="https://github.com/Aliaksandr-Bychyk" target="_blank">
+      Aliaksandr Bychyk
+    </a>
+    .
   </div>
 );
 
