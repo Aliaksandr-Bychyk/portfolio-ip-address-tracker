088c242b8cf4990c07eed7ca0c2bed029117b670
Aliaksandr <sanya55bychyk@gmail.com>
2023-11-02 15:36:42 +0100

    feat: update Layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -3,25 +3,32 @@ import './Layout.css';
 
 const Layout: FC = () => (
   <div className="layout">
-    <h1>IP Address Tracker</h1>
-    <div>
-      <input type="text" placeholder="Search for any IP address or domain" />
-      <input type="button" />
+    <h1 className="title">IP Address Tracker</h1>
+    <div className="search-box">
+      <input
+        type="text"
+        className="input-search"
+        placeholder="Search for any IP address or domain"
+      />
+      <input type="button" className="input-submit" />
     </div>
-    <ul>
-      <li>
+    <ul className="control-panel">
+      <li className="control-panel__element">
         <div>IP Address</div>
         <div>0.0.0.0</div>
       </li>
-      <li>
+      <div className="vl"></div>
+      <li className="control-panel__element">
         <div>Location</div>
         <div>Brooklyn, NY 10001</div>
       </li>
-      <li>
+      <div className="vl"></div>
+      <li className="control-panel__element">
         <div>Timezone</div>
         <div>UTC-5:00</div>
       </li>
-      <li>
+      <div className="vl"></div>
+      <li className="control-panel__element">
         <div>ISP</div>
         <div>SpaceX Starlink</div>
       </li>
